6b714fb0c60b4e553c4c084d39fdce9745ad53ef
alexnavtt <alexnavtt@utexas.edu>
2022-01-28 14:49:35 -0600

    changed output to compare tire displacement instead. Also normalized by rms

--- Lab 2 - System Modeling/plotLinearSystemComparision.m
+++ Lab 2 - System Modeling/plotLinearSystemComparision.m
@@ -4,12 +4,12 @@ close all
 
 %% Load the data from the TDMS file
 disp   = cell(4,1);
-acc    = cell(4,1);
+tire   = cell(4,1);
 zr_dot = cell(4,1);
 T      = cell(4,1);
 for i=1:4
 	name = ['Active Suspension/TDMS/suspensiondata' num2str(i) '.tdms'];
-	[disp{i}, acc{i}, zr_dot{i}, T{i}] = TDMSdata(name, 4);
+	[disp{i}, tire{i}, zr_dot{i}, T{i}] = TDMSdata(name, 4);
 	
 	% Make time start at 0
 	T{i} = T{i} - T{i}(1);
@@ -26,6 +26,7 @@ for i = 1:4
 	sim_disp = yout.getElement('suspension_travel').Values.Data;
 	sim_acc  = yout.getElement('body_acc').Values.Data;
 	sim_zrd  = yout.getElement('zr_dot').Values.Data;
+	sim_tire = yout.getElement('tire_displacement').Values.Data;
 	
 	figure(i)
 	
@@ -36,17 +37,20 @@ for i = 1:4
 	plot(sim_time, sim_zrd)
 	legend('Measured', 'Simulated')
 	title('Input Road Velocity')
+	xlim([0 tmax])
 	
 	% Compare the measured data with the simulated data
 	subplot(3,1,2)
 	hold on
-	plot(T{i}, disp{i}/median(abs(disp{i})))
-	plot(sim_time, sim_disp/median(abs(sim_disp)))
-	title('Displacement')
+	plot(T{i}, disp{i}/rms(disp{i}))
+	plot(sim_time, sim_disp/rms(sim_disp))
+	title('Suspension Travel')
+	xlim([0 tmax])
 	
 	subplot(3,1,3)
 	hold on
-	plot(T{i}, acc{i}/max(acc{i}))
-	plot(sim_time, sim_acc/median(abs(sim_acc)))
-	title('Acceleration')
+	plot(T{i}, tire{i}/rms(tire{i}))
+	plot(sim_time, sim_tire/rms(sim_tire))
+	title('Tire Displacement')
+	xlim([0 tmax])
 end
