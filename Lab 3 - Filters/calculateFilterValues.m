7b6ce0707211add57ecae93584c25805565eba34
alexnavtt <alexnavtt@utexas.edu>
2022-02-12 12:39:45 -0600

    changes made during lab

--- Lab 3 - Filters/calculateFilterValues.m
+++ Lab 3 - Filters/calculateFilterValues.m
@@ -1,14 +1,14 @@
 clc
-wc = 30;			% Hz
+wc = 8;			% Hz
 wc = wc * 2 * pi;	% rad/s
 zeta = 0.6;
 
 %% First order filter
 tau = 1/wc;
 fprintf('First order transfer function:')
-tf(1, [tau 1])
+H1 = tf(1, [tau 1])
 
 %% Second order filter
-wd = wc/sqrt(1-2*zeta^2);
+% wd = wc/sqrt(1-2*zeta^2);
 fprintf('Second order transfer function:')
-tf(wc^2, [1 2*zeta*wc wc^2])
+H2 = tf(wc^2, [1 2*zeta*wc wc^2])
