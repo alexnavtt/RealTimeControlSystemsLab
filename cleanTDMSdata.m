354cba2603713f5904f1a9a4899cf15c185cc138
alexnavtt <alexnavtt@utexas.edu>
2022-02-12 12:39:22 -0600

    file to clean up tdms data

---
+++ cleanTDMSdata.m
@@ -0,0 +1,36 @@
+function [varargout] = cleanTDMSdata(filepath, num_channels)
+
+messy_data = cell(num_channels,1);	% Input data
+varargout  = cell(num_channels,1);	% Output cleaned data
+
+% Read the data from the TDMS file
+[messy_data{:}] = TDMSdata(filepath, num_channels);
+for i = 1:num_channels
+	varargout{i}(1) = messy_data{i}(1);
+end
+data_len = length(messy_data{end});
+
+% Loop through and record data whenever it changes
+idx = 1;
+for i = 2:data_len
+	% Determine if data has changed to a new timestep
+	changed = false;
+	for j = 1:num_channels
+		if messy_data{j}(i) ~= messy_data{j}(i-1)
+			changed = true;
+			break;
+		end
+	end
+	
+	% If it has changed, record the new value in the output arrays
+	if changed
+		for j = 1:num_channels
+			varargout{j}(idx) = messy_data{j}(i);
+		end
+		idx = idx + 1;
+	end
+end
+
+for i = 1:num_channels
+	varargout{i} = varargout{i}(1:idx-1)';
+end
