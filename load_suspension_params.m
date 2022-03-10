ee5ade55914284dea27de3e62b2ab073a64552d4
alexnavtt <alexnavtt@utexas.edu>
2022-03-10 16:47:54 -0600

    Simulation files for Lab 4

---
+++ load_suspension_params.m
@@ -0,0 +1,22 @@
+Ms  = 2.45; % kg
+Mus = 1.00; % kg
+Ks  = 900;  % N/m
+Kus = 1250; % N/m
+Bs  = 7.5;  % N s/m
+Bus = 5.0;  % N s/m
+
+A = [  0         1         0            -1        ;
+	 -Ks/Ms   -Bs/Ms       0           Bs/Ms      ;
+	    0        0         0             1        ;
+	 Ks/Mus   Bs/Mus   -Kus/Mus   -(Bs + Bus)/Mus];
+ 
+B = [    0       0   ;
+	     0      1/Ms ;
+	    -1       0   ;
+	  Bus/Mus -1/Mus];
+  
+C = [   1      0    0     0   ;
+     -Ks/Ms -Bs/Ms  0   Bs/Ms];
+
+D = [ 0   0   ;
+	  0  1/Ms];
