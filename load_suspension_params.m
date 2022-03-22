3ab07da04a3d864a3175cb292a0a229432302ffc
alexnavtt <alexnavtt@utexas.edu>
2022-03-22 16:36:25 -0500

    file to load active suspension model

--- load_suspension_params.m
+++ load_suspension_params.m
@@ -1,9 +1,9 @@
-Ms  = 2.45; % kg
-Mus = 1.00; % kg
-Ks  = 900;  % N/m
-Kus = 1250; % N/m
-Bs  = 7.5;  % N s/m
-Bus = 5.0;  % N s/m
+Ms  = 2.45; %2.45; % kg
+Mus = 1.00; %1.00; % kg
+Ks  = 1000; %900;  % N/m
+Kus = 1250; %1250; % N/m
+Bs  = 15; %7.5;  % N s/m
+Bus = 15; %5.0;  % N s/m
 
 A = [  0         1         0            -1        ;
 	 -Ks/Ms   -Bs/Ms       0           Bs/Ms      ;
